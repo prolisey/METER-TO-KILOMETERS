fa25d3f18cea2ebaa5d8451bab26e92afe64ce59
بييبلاتنمك <83167483+prolisey@users.noreply.github.com>
2023-03-19 15:18:41 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function o() {
+    let w =  document.getElementById('w').value;
+    let i = w /1000;
+    let a = w * 100;
+    let b = w * 1000;
+    document.getElementById('i').innerText = i;
+    document.getElementById('a').innerText = a;
+    document.getElementById('b').innerText = b;
+}
